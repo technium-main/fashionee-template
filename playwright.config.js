d9baaa70d38a34a6a4a84939a82a132836234023
yuliya.prokofieva <yuliya.prokofieva@ladadigit.io>
2025-09-30 22:28:25 +0500

    [#14] - Фиксы после тестирования

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './__tests__/e2e',
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
+
   use: {
     baseURL: 'http://localhost:5173',
     headless: true,
