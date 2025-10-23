531385cd964f55175f81e6cbf25ecae39c58fdb2
Sergei Shologin <s.shologin@yandex.ru>
2025-10-23 11:25:18 +0500

    Add branch name validation to the CI workflow, disable ESLint error in config

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev',
     url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI, // eslint-disable-line no-undef
     timeout: 120000,
   },
 
